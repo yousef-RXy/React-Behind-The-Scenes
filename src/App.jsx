5e4c43d0b6c41a728b415dc825db5ab9f2da952d
Yousef Na <yousefnasr0541@gmail.com>
2023-11-01 08:57:01 +0200

    prevent unnecessary component re-render

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
+import ConfigureCounter from "./components/Counter/ConfigureCounter.jsx";
 
 function App() {
-  log('<App /> rendered');
+	log("<App /> rendered");
 
-  const [enteredNumber, setEnteredNumber] = useState(0);
-  const [chosenCount, setChosenCount] = useState(0);
+	const [chosenCount, setChosenCount] = useState(0);
 
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
-  }
+	function handleSetClick(num) {
+		setChosenCount(num);
+	}
 
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
-  }
-
-  return (
-    <>
-      <Header />
-      <main>
-        <section id="configure-counter">
-          <h2>Set Counter</h2>
-          <input type="number" onChange={handleChange} value={enteredNumber} />
-          <button onClick={handleSetClick}>Set</button>
-        </section>
-        <Counter initialCount={chosenCount} />
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main>
+				<ConfigureCounter onClick={handleSetClick} />
+				<Counter initialCount={chosenCount} />
+			</main>
+		</>
+	);
 }
 
 export default App;
